9fe8e3fd900af7b64a167be379a28c667610a7e1
Paul Norberger <hi@paulnorberger.de>
2025-10-04 16:50:06 +0200

    Make button prompt sprite invisible via code

--- godot/interactable/interactable.gd
+++ godot/interactable/interactable.gd
@@ -4,6 +4,7 @@ extends Area2D
 func _ready() -> void:
 	body_entered.connect(_on_player_entered)
 	body_exited.connect(_on_player_exited)
+	$ButtonPromptSprite.visible = false
 
 func on_trigger(): # overwrite in child scenes & scripts
 	print("Trigger pressed, but behavior not overwritten!")
