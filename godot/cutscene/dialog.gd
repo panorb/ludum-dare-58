bb655d5d7d7b62790e9bd06faa47b39ccf18b731
Paul Norberger <hi@paulnorberger.de>
2025-10-05 09:59:59 +0200

    Speech bubble minimum size finally works

--- godot/cutscene/dialog.gd
+++ godot/cutscene/dialog.gd
@@ -1,7 +1,10 @@
 class_name Dialog
 extends Control
 
+@export var speech_bubble_scene : PackedScene = preload("res://cutscene/speech_bubble.tscn")
 var current_speaker_node : Node2D
+
+var current_speech_bubble_node : SpeechBubble
 
 func display_bubble(text: String, speaker: String):
 	# TODO: Handle speaker
@@ -9,13 +12,20 @@ func display_bubble(text: String, speaker: String):
 	assert(len(speaker_candidates) == 1)
 	current_speaker_node = speaker_candidates[0]
 	
-	$SpeechBubble.visible = true
-	$SpeechBubble.text = text
-	force_update_transform()
+	var old_speech_bubble_node = current_speech_bubble_node
+	current_speech_bubble_node = speech_bubble_scene.instantiate()
+	add_child(current_speech_bubble_node)
+	current_speech_bubble_node.text = text
+	
+	if old_speech_bubble_node:
+		old_speech_bubble_node.queue_free()
+	
 
 func hide_bubble():
-	$SpeechBubble.visible = false
+	current_speech_bubble_node.queue_free()
+	current_speech_bubble_node = null
+	# $SpeechBubble.visible = false
 
 func _process(delta: float) -> void:
-	if current_speaker_node:
-		$SpeechBubble.position = current_speaker_node.global_position
+	if current_speaker_node and current_speech_bubble_node:
+		current_speech_bubble_node.position = current_speaker_node.global_position
