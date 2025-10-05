b6ade2303020f04076f8437e3459e4cfe8baa23b
Phahou <38053767+Phahou@users.noreply.github.com>
2025-10-05 12:43:20 +0200

    wip working out the data in/outputs

--- godot/cutscene/dialog.gd
+++ godot/cutscene/dialog.gd
@@ -26,14 +26,36 @@ func hide_bubble():
 	current_speech_bubble_node = null
 	# $SpeechBubble.visible = false
 
+
+var skip = 0
+func iter_print(msg):
+	if skip % 4 == 0:
+		print(msg)
+
 func _process(delta: float) -> void:
-	var camera : Camera2D = get_tree().get_first_node_in_group("camera")
+	skip = skip + 1
+	var player : CharacterBody2D = get_tree().get_first_node_in_group("player")
+	var camera: Camera2D = get_tree().get_first_node_in_group("camera")
+	var t = get_viewport_rect()
+	iter_print(t)
+	# print("t: {0}".format([t]))
+	iter_print("camera:\n\tviewport_rect: {0}\n\ttransform: {1}".format([camera.get_viewport_rect(),  camera.get_viewport_transform()]))
+	iter_print("player: P:{0}, GP:{1}".format([player.position,player.global_position]))
 	
-	var camera_topleft = camera.global_position - (get_window().size / 2.0)
-	print(camera_topleft)
+	#if current_speaker_node:
+	#	print("speaker global pos: {0}".format([current_speaker_node.global_position]))
+	
+	# var camera_topleft = camera.global_position - (get_window().size / 2.0)
+	# print(camera_topleft)
 	
 	if current_speaker_node and current_speech_bubble_node:
-		var viewport_pos = get_viewport().get_visible_rect().position
-		current_speech_bubble_node.position = current_speaker_node.global_position - viewport_pos
+		var origin = camera.get_viewport_transform().origin
+		iter_print("cspn:\n\tp: {0}\n\t gp: {1}".format([current_speaker_node.position, current_speaker_node.global_position]))
+		current_speech_bubble_node.position.x = current_speaker_node.position.x - origin.x
+		current_speech_bubble_node.position.y = current_speaker_node.position.y - origin.y
+		iter_print("csbn: {0}".format([current_speech_bubble_node.position.x]))
+		# var viewport_pos = get_viewport().get_visible_rect().position
+		# print(t)
+		# current_speech_bubble_node.position = current_speaker_node.global_position
 		# current_speech_bubble_node.position = current_speaker_node.global_position # + camera.global_position + (get_window().size / 2.0)
 	
