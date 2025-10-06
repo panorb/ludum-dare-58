a5a3cdf1ea0267edcc218c3052ec551ba2ead411
Paul Norberger <hi@paulnorberger.de>
2025-10-06 22:41:31 +0200

    Add grav generator sfx

--- godot/level/decals/grav_gen.gd
+++ godot/level/decals/grav_gen.gd
@@ -4,7 +4,11 @@ func _ready() -> void:
 	fast()
 
 func slow():
+	$HighNoisePlayer.stop()
+	$LowNoisePlayer.play()
 	$Sprite2D.play("slow")
 
 func fast():
+	$LowNoisePlayer.stop()
+	$HighNoisePlayer.play()
 	$Sprite2D.play("fast")
