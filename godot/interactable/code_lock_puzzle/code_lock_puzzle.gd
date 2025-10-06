a9952afc276d99b0295603154fab26de77e57bb8
lucas <nickfleming501@protonmail.com>
2025-10-06 16:59:14 +0200

    Added button puzzle

--- godot/interactable/code_lock_puzzle/code_lock_puzzle.gd
+++ godot/interactable/code_lock_puzzle/code_lock_puzzle.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var correct_combination : String = "123"
-@export var on_puzzle_solve_group : String = ""
+@export var on_puzzle_solve_group : String = "platform"
 
 @onready var number_buttons : Array[NumberButton] = [$NumberButton1, $NumberButton2, $NumberButton3]
 var current_combination = "AAA"
