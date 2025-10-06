e5df868ad0dd0edd67502e4943385c880dfbbb3d
Paul Norberger <hi@paulnorberger.de>
2025-10-06 10:32:25 +0200

    Add hose item & something else i dont remember

--- godot/game/inventory_item.gd
+++ godot/game/inventory_item.gd
@@ -2,7 +2,8 @@ extends Control
 
 @export var item_icons : Dictionary = {
 	"wrench": preload("res://interactable/wrench.png"),
-	"id_card": preload("res://ui/inventory_items/id_card.png")
+	"id_card": preload("res://ui/inventory_items/id_card.png"),
+	"hose": preload("res://ui/inventory_items/hose.png")
 }
 
 func set_item(item):
